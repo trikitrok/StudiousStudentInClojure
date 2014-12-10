c52c299e878a7e66083a236a6489ac1bb27db1d8
trikitrok <trikitrok@gmail.com>
2014-12-11 00:39:27 +0100

    I did a spike on the REPL to get a list of words lists from a file

--- studios_student_repl.clj
+++ studios_student_repl.clj
@@ -2,6 +2,12 @@
 
 (apply str (sort words-list))
 
+(apply str (sort ["uiuy" "hopji" "li" "j" "dcyi"])) ; dcyihopjijliuiuy
+
+(apply str
+       (sort #(compare (str %1 %2) (str %2 %1))
+             ["uiuy" "hopji" "li" "j" "dcyi"]))
+
 (sort #(compare (str %1 %2) (str %2 %1))
       words-list)
 
@@ -9,4 +15,23 @@
        (sort #(compare (str %1 %2) (str %2 %1))
              words-list));bwjibwjibwjijp
 
-;(lexic-shortest-concat
+(rest (clojure.string/split-lines (slurp "test/studious_student/studious_student.in")))
+
+(clojure.string/split "9 rgh woqg dmabatgbt qrvpcrx eluunoi sy w wnthqxgkg aimallazuc" #" ")
+
+(map #(rest (clojure.string/split % #" "))
+     (rest (clojure.string/split-lines
+            (slurp "test/studious_student/studious_student.in"))))
+
+
+
+(defn lexic-shortest-concat [words-list]
+  (apply str
+         (sort
+           #(compare (str %1 %2) (str %2 %1))
+           words-list)))
+
+(map lexic-shortest-concat
+(map #(rest (clojure.string/split % #" "))
+     (rest (clojure.string/split-lines
+            (slurp "test/studious_student/studious_student.in")))))
